165c929c02781244698ae94fd29103906b0c9ab1
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-07-21 10:00:06 +0000

    Jul 21, 2025, 11:00 AM

--- test.php
+++ test.php
@@ -12,4 +12,9 @@ echo "You are a teenager";
 for ($i=0; $i < 5; $i++) {
 echo "This is loop number $i\n";
 }
+
+$var= array(1, 2, 3, 4, 5);
+foreach ($var as $value) {
+    echo "Value: $value\n";
+}
 ?>
