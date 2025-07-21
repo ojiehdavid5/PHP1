610b3f460a6ba5ae4b1f8df341966aae10f324f6
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-07-21 09:51:42 +0000

    Jul 21, 2025, 10:51 AM

--- test.php
+++ test.php
@@ -3,8 +3,10 @@ $int=8;
 $float=8.0;
 echo "Integer: $int\n";
 echo "Float: $float\n";
-$age = 18;
+$age = 80;
 if ($age < 20) {
 echo "You are a teenager";
+}else{
+    echo "You are an adult";
 }
 ?>
