bced545e32226ece2942b4788a3ee73d1515836b
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-07-21 10:19:08 +0000

    Jul 21, 2025, 11:19 AM

--- test.php
+++ test.php
@@ -12,15 +12,12 @@ echo "You are a teenager";
 for ($i=0; $i < 5; $i++) {
 echo "This is loop number $i\n";
 }
-
-$var= array(1, 2, 3, 4, 5);
-foreach ($var as $value) {
-    echo "Value: $value\n";
-}
-
 $namesAge = array("Fabio"=>"20"
 ,
 "Klevi"=>"16"
 ,
-"John"=>"43"); 
+"John"=>"43");
+echo "Fabio’s age is "
+. $namesAge['Fabio'] . " years old.
+";
 ?>
