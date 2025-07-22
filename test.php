2a9c1f3a571f8bfd6ff56826afdc5f3f00499e4b
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-07-22 09:45:48 +0000

    Jul 22, 2025, 10:45 AM

--- test.php
+++ test.php
@@ -1,25 +1,25 @@
 <?php
-$int=8;
-$float=8.0;
-echo "Integer: $int\n";
-echo "Float: $float\n";
-$age = 80;
-if ($age < 20) {
-echo "You are a teenager";
-}else{
-    echo "You are an adult";
-}
-for ($i=0; $i < 5; $i++) {
-echo "This is loop number $i\n";
-}
-$namesAge = array("Fabio"=>"20"
-,
-"Klevi"=>"16"
-,
-"John"=>"43");
-echo "Fabio’s age is "
-. $namesAge['Fabio'] . " years old.
-";
+// $int=8;
+// $float=8.0;
+// echo "Integer: $int\n";
+// echo "Float: $float\n";
+// $age = 80;
+// if ($age < 20) {
+// echo "You are a teenager";
+// }else{
+//     echo "You are an adult";
+// }
+// for ($i=0; $i < 5; $i++) {
+// echo "This is loop number $i\n";
+// }
+// $namesAge = array("Fabio"=>"20"
+// ,
+// "Klevi"=>"16"
+// ,
+// "John"=>"43");
+// echo "Fabio’s age is "
+// . $namesAge['Fabio'] . " years old.
+// ";
 
 
 //function in php
